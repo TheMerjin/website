776f214510b522ee42afc98efb651ca398a989a8
TheMerjin <sreekarc11@gmail.com>
2025-05-25 19:18:05 -0700

    Here added backend validation of AUTH's

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,29 @@
+// With `output: 'static'` configured:
+export const prerender = false;
+import type { APIRoute } from "astro";
+import { supabase } from '../../../lib/supabase.js';
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+
+  if (!email || !password) {
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+    if (error) {
+        if (error.message === "User already registered") {
+            // Redirect user to /signin
+            return Response.redirect("http://localhost:4321/login?msg=User%20already%20registered", 303);
+          }
+    return new Response(error.message, { status: 500 });
+  }
+
+  return redirect("http://localhost:4321/login?msg=User%20already%20registered");
+};
