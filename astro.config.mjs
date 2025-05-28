ffeeb8fbb5c29e5c89d5c5ed58cd87e82df9dc97
TheMerjin <sreekarc11@gmail.com>
2025-05-28 13:53:11 -0700

    Added dynamic posting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react()],
+  output: 'server', // <--- MUST BE PRESENT
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
