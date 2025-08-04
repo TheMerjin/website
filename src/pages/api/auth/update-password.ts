c22f28e538700258f2ed9727a5d4f6ef0f26fd6b
TheMerjin <sreekarc11@gmail.com>
2025-08-03 19:10:52 -0700

    hello

--- src/pages/api/auth/update-password.ts
+++ src/pages/api/auth/update-password.ts
@@ -2,12 +2,14 @@ import { supabase } from '../../../lib/client-supabase';
 import type { APIRoute } from 'astro';
 
 export const POST: APIRoute = async ({ request }) => {
-  const { password, accessToken } = await request.json();
-
+  const { password, accessToken, refreshToken } = await request.json();
+  supabase.auth.setSession({ access_token: accessToken, refresh_token: refreshToken })
   const { error } = await supabase.auth.updateUser(
     { password },
     { accessToken }
   );
+  
+  console.log(error);
 
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
